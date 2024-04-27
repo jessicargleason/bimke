c82ecf3d7e0110c4fed87873e4ef2a3aa3d98542
Jessica Gleason <jessicaruthgleason@gmail.com>
2024-04-26 19:58:21 -0500

    Set up news template and news items

--- src/content/config.js
+++ src/content/config.js
@@ -4,19 +4,34 @@ import { z, defineCollection } from 'astro:content';
 const bookCollection = defineCollection({
     type: "content",
     schema: z.object({
-        bookTitle: z.string(),
-        bookType: z.string().optional(),
-        coverImage: z.string(),
-        eventLink: z.string().optional(),
-        archiveLink: z.string(),
-        eventDateHeading: z.string().optional(),
-        archived: z.boolean(),
-        year: z.number().optional(),
-        monthNumber: z.number().optional(),
+      bookTitle: z.string(),
+      bookType: z.string().optional(),
+      coverImage: z.string(),
+      eventLink: z.string().url().optional(),
+      archiveLink: z.string().url(),
+      eventDateHeading: z.string().optional(),
+      archived: z.boolean(),
+      year: z.number().optional(),
+      monthNumber: z.number().optional(),
     })
+});
+
+const newsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }).optional(),
+    link: z.string().url(),
+    sourceName: z.string(),
+  })
 });
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   'books': bookCollection,
+  'news': newsCollection,
 };
---
+++ src/content/news/2019/pridefest.md
@@ -0,0 +1,12 @@
+---
+title: PrideFest Milwaukee honors our community heroes
+date: 2019-06-04
+link: https://urbanmilwaukee.com/pressrelease/pridefest-milwaukee-honors-our-community-heroes/
+sourceName: Urban Milwaukee
+image: {
+    url: pridefest.jpg,
+    alt: Rise! Pridefest Milwaukee June 6-9 2019
+}
+---
+
+Urban Milwaukee reports on Bi+ Pride Milwaukee receiving a Pride Award at Pridefest 2019.
